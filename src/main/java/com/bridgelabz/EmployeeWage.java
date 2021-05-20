84b2d324f62cb4d898425d69c8789cbc8cabab07
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-20 14:53:30 +0530

    ADDED: functionality to find wage for part time

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -1,14 +1,23 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    private static final int IS_PRESENT = 1;
+    private static final int  WAGE_PER_HR = 20;
+    private static final int IS_PART_TIME = 2;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        final int IS_PRESENT = 1;
-        final int  WAGE_PER_HR = 20;
+
         int empHR = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        System.out.println(empCheck);
+        //full time
         if(empCheck == IS_PRESENT) {
             empHR = 8;
+        }
+        //part time
+        if(empCheck == IS_PART_TIME) {
+            empHR = 4;
         }
         int empWage = WAGE_PER_HR * empHR;
         System.out.println("Employee wage: " + empWage);
