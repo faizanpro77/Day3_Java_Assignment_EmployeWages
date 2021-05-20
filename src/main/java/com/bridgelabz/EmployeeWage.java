55fbc72ac8fef001bb4aa77a54344aa31b3d30b4
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-20 18:44:12 +0530

    ADDED: functionality toCalculating Wages

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -2,29 +2,30 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     private static final int IS_Full_TIME = 1;
-    private static final int  WAGE_PER_HR = 20;
+    private static final int WAGE_PER_HR = 20;
     private static final int IS_PART_TIME = 2;
 
     public static void main(String[] args) {
 
         int empHR = 0;
+        int empWage=0;
+        for (int day = 1 ; day <= 20 ; day ++) {
 
-        int empCheck = (int) Math.floor(Math.random()*10)%3;
-        System.out.println(empCheck);
-
-        switch (empCheck) {
-            case IS_Full_TIME:
-                empHR = 8;
-                break;
-            case IS_PART_TIME:
-                empHR = 4;
-                break;
-            default:
-                empHR = 0;
-                break;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_Full_TIME:
+                    empHR = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHR = 4;
+                    break;
+                default:
+                     empHR = 0;
+                     break;
+            }
+            empWage = empWage + (WAGE_PER_HR * empHR);
         }
-        int empWage = WAGE_PER_HR * empHR;
-        System.out.println("Employee wage: " + empWage);
+        System.out.println("Employee monthly wage: " + empWage);
     }
 }
 
