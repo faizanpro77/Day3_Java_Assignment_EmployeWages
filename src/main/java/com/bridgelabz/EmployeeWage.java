ed67e1f25ae3b27c92e4d0e5f7efb625e4eaf073
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-23 15:32:00 +0530

    REFACTORED: use method to calculate employee wage

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -1,40 +1,38 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    private static final int IS_Full_TIME = 1;
-    private static final int WAGE_PER_HR = 20;
-    private static final int IS_PART_TIME = 2;
-    private static final int MAX_HR = 100;
-    private static final int MAX_DAY = 20;
+    private static String company;
 
-    public static void main(String[] args) {
-
+    public static void employeeWageForCompany(String company, int wagePerHour, int maxHr, int maxDay) {
+       // EmployeeWage.company = company;
         int empHR = 0;
         int empWage=0;
         int totalWorkingHr = 0;
         int day = 0;
-        while (totalWorkingHr < MAX_HR && day < MAX_DAY){
+        while (totalWorkingHr < maxHr && day < maxDay){
 
             int empCheck = (int) (Math.floor(Math.random() * 10)) % 3;
 
             switch (empCheck) {
-                case IS_Full_TIME:
+                case 1:
                     empHR = 8;
                     break;
-                case IS_PART_TIME:
+                case 2:
                     empHR = 4;
                     break;
                 default:
                      empHR = 0;
                      break;
             }
-            empWage = empWage + (WAGE_PER_HR * empHR);
+            empWage = empWage + (wagePerHour * empHR);
             day++;
             totalWorkingHr = totalWorkingHr + empHR;
         }
-        System.out.println("Employee monthly wage: " + empWage);
+        System.out.println("Employee monthly wage for company " + company + " : " + empWage);
         System.out.println("Employee total working hours: " + totalWorkingHr);
-
+    }
+    public static void main(String[] args){
+        employeeWageForCompany("Dmart",20,30,20);
     }
 }
 
