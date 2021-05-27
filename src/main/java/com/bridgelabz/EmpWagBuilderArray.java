329653e89c2bc32fa9e47fa607b3629451f36948
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-28 01:28:58 +0530

    MODIFIED: add functionality queried by Company

--- src/main/java/com/bridgelabz/EmpWagBuilderArray.java
+++ src/main/java/com/bridgelabz/EmpWagBuilderArray.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Scanner;
 
 public class EmpWagBuilderArray implements EmpWageInterface {
 
@@ -36,19 +36,43 @@ public class EmpWagBuilderArray implements EmpWageInterface {
         }
         System.out.println( "Total Wage Of " + companyEmpWage.companyName + " Employee is: " + companyEmpWage.empMonthlyWages );
     }
+    public static void queryOfCompany(){
 
+        EmpWagBuilderArray empWageBuilder = new EmpWagBuilderArray();
+        Scanner scanner = new Scanner(System.in);
+        //Arraylist
+        ArrayList<CompanyEmpWage> company = new ArrayList<>();
+        System.out.println("Query of company employee wage");
+        System.out.println("1 for Dmart");
+        System.out.println("2 for Big Basket");
+        System.out.println("3 for Reliance") ;
+        int choice = Integer.valueOf(scanner.next());
+        switch(choice) {
+            case 1:
+                CompanyEmpWage wage1 = new CompanyEmpWage("MartD",15,50,25);
+                company.add(wage1);
+                empWageBuilder.calculateEmpWage(wage1);
+                break;
+            case 2:
+                CompanyEmpWage wage2 = new CompanyEmpWage("Big Basket",26,65,33);
+                company.add(wage2);
+                empWageBuilder.calculateEmpWage(wage2);
+                break;
+            case 3:
+                CompanyEmpWage wage3 = new CompanyEmpWage("Reliance",25,45,30);
+                company.add(wage3);
+                empWageBuilder.calculateEmpWage(wage3);
+                break;
+            default:
+                System.out.println("Invalid Option choose write option");
+                System.out.println("1 for Dmart");
+                System.out.println("2 for Big Basket");
+                System.out.println("3 for Reliance") ;
+                break;
+        }
+    }
     public static void main(String[] args) {
         System.out.println( " Welcome to Employee Wage Computation Program " );
-        EmpWagBuilderArray empWageBuilder = new EmpWagBuilderArray();
-        CompanyEmpWage wage1 = new CompanyEmpWage("MartD",15,50,25);
-        CompanyEmpWage wage2 = new CompanyEmpWage("Big Basket",26,65,33);
-        CompanyEmpWage wage3 = new CompanyEmpWage("Reliance",25,45,30);
-        ArrayList<CompanyEmpWage> list = new ArrayList<>();
-        list.add(wage1);
-        list.add(wage2);
-        list.add(wage3);
-        empWageBuilder.calculateEmpWage(wage1);
-        empWageBuilder.calculateEmpWage(wage2);
-        empWageBuilder.calculateEmpWage(wage3);
+        queryOfCompany();
     }
 }
